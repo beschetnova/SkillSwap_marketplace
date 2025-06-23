6a285b767f0ce06705c8bf15aaa61ad9c922f0f8
Vladimir Moizes <vova3x20@gmail.com>
2025-06-24 01:03:59 +0300

    функциональный и UI компоненты модалки

---
+++ src/components/ui/modal/modal.tsx
@@ -0,0 +1,20 @@
+import type { FC } from 'react';
+import type { TModalUIProps } from './type';
+import styles from './modal.module.css';
+
+export const ModalUI: FC<TModalUIProps> = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className={styles.overlay} onClick={onClose}>
+      <div
+        className={styles.modal}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
