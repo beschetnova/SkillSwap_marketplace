9e653de7682fbbd9643da8ec787f63787d35c7f4
JEFFors <33591723+jeffors@users.noreply.github.com>
2025-06-23 20:33:18 +0500

    API to fetch skills

---
+++ src/api/api.ts
@@ -0,0 +1,13 @@
+import type { SkillCategories } from '../utils/types';
+
+const URL = process.env.PUBLIC_URL;
+
+export const fetchSkillAPI = async (): Promise<SkillCategories> => {
+  const response = await fetch(`${URL}/db/skills.json`);
+  if (!response.ok) {
+    throw new Error('Не удалость загрузить скиллы. ' + response.status);
+  }
+
+  const data: SkillCategories = await response.json();
+  return data;
+};
