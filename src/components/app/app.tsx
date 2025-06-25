13b3837fc436b36328bd1790a553c45095f25116
JEFFors <33591723+jeffors@users.noreply.github.com>
2025-06-25 15:36:24 +0500

    Add cities slice

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,11 +3,13 @@ import { useAppDispatch } from '../../utils/hooks';
 import { fetchSkills } from '../../services/slices/skillsSlice';
 import { fetchUsers } from '../../services/slices/usersSlice';
 import Aside from '../Aside/Aside';
+import { fetchCities } from '../../services/slices/citiesSlice';
 function App() {
   const dispatch = useAppDispatch();
   useEffect(() => {
     dispatch(fetchSkills());
     dispatch(fetchUsers());
+    dispatch(fetchCities())
   }, []);
 
   return (
--- src/services/store.ts
+++ src/services/store.ts
@@ -1,15 +1,17 @@
 import { combineSlices, configureStore } from '@reduxjs/toolkit';
 import skillsSlice from './slices/skillsSlice';
 import usersSlice from './slices/usersSlice';
+import citiesSlice from './slices/citiesSlice';
 
 export const rootReducer = combineSlices({
   skills: skillsSlice,
-  users: usersSlice
+  users: usersSlice,
+  cities: citiesSlice
 });
 
 const store = configureStore({
   reducer: rootReducer,
-  // devTools: process.env.NODE_ENV !== 'production'
+  devTools: process.env.NODE_ENV !== 'production'
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
