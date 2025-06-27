970641e8f244858790694ba7c52e4b66521c7831
vnechit <vnechit310301@gmail.com>
2025-06-27 23:28:03 +0300

    some fixes

--- src/components/ui/FilterNested/FilterNested.tsx
+++ src/components/ui/FilterNested/FilterNested.tsx
@@ -4,6 +4,7 @@ import chevron from '../../../images/icons/chevron-down.svg';
 import clsx from 'clsx';
 import emptyCheckBox from '../../../images/icons/checkbox-empty.svg';
 import filledCheckBox from '../../../images/icons/checkbox-done.svg';
+import deleteCheckBox from '../../../images/icons/checkbox-remove.svg';
 
 type Props = {
   title?: string;
@@ -15,6 +16,8 @@ type Props = {
   toggleCategoryExpand: (categoryId: string) => void;
   toggleSkillCheck: (skillId: string) => void;
   buttonName: string;
+  markSkill: (categoryId: string) => void;
+  unMarkSkill: (categoryId: string) => void;
 };
 
 const FilterNestedUI = ({
@@ -26,28 +29,93 @@ const FilterNestedUI = ({
   toggleShowAll,
   toggleCategoryExpand,
   toggleSkillCheck,
-  buttonName
+  buttonName,
+  markSkill,
+  unMarkSkill
 }: Props) => {
   const visibleItems = showAll ? items : items.slice(0, 5);
+
+  const checkIfChecked = (categoryId: number) => {
+    let count = 0;
+    items[categoryId].skills.forEach((skill) => {
+      if (checkedItems.includes(skill.id)) {
+        count++;
+      }
+    });
+
+    if (count === 0) {
+      return -1;
+    } else if (count < items[categoryId].skills.length) {
+      return 0;
+    }
+    return 1;
+  };
+
+  const unMarkAllCategory = (categoryId: number): void => {
+    items[categoryId].skills.forEach((skill) => {
+      unMarkSkill(skill.id);
+    });
+  };
+
+  const markAllCategory = (categoryId: number): void => {
+    items[categoryId].skills.forEach((skill) => {
+      markSkill(skill.id);
+    });
+  };
 
   return (
     <div className={styles.filterContainer}>
       {title && <h3 className={styles.title}>{title}</h3>}
-      {visibleItems.map((category) => (
+      {visibleItems.map((category, id) => (
         <ul key={category.id} className={styles.mainList}>
           {/* Категория с кнопкой раскрытия */}
           <li className={styles.mainListPoint}>
-            <button onClick={() => toggleCategoryExpand(category.id)}>
-              {expandedCategories.includes(category.id) ? '-' : '+'}
-            </button>
-            <span className={styles.mainListCategory}>{category.name}</span>
+            <input type='checkbox' className={styles.checkboxInput} />
+            {checkIfChecked(id) === -1 && (
+              <img
+                src={emptyCheckBox}
+                onClick={() => {
+                  markAllCategory(id);
+                }}
+              />
+            )}
+            {checkIfChecked(id) === 0 && (
+              <img
+                src={deleteCheckBox}
+                onClick={() => {
+                  unMarkAllCategory(id);
+                }}
+              />
+            )}
+            {checkIfChecked(id) === 1 && (
+              <img
+                src={filledCheckBox}
+                onClick={() => {
+                  unMarkAllCategory(id);
+                }}
+              />
+            )}
+            <span
+              className={styles.mainListCategory}
+              onClick={() => toggleCategoryExpand(category.id)}
+            >
+              {category.name}
+            </span>
+            <img
+              src={chevron}
+              className={clsx({
+                [styles.dNone]: !expandedCategories.includes(category.id),
+                [(styles.bottomButtonIconReverse, styles.bottomButtonIcon)]:
+                  expandedCategories.includes(category.id)
+              })}
+            />
           </li>
           {/* Внутри раскрытой категории список навыков */}
           {expandedCategories.includes(category.id) && (
             <ul className={styles.extraList}>
               {category.skills.map((skill) => (
                 <li key={skill.id}>
-                  <label className={styles.mainListPoint}>
+                  <label className={styles.extraListPoint}>
                     <input
                       type='checkbox'
                       checked={checkedItems.includes(skill.id)}
