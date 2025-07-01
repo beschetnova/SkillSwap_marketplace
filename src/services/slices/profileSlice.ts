b502bea92c166bdc167768ed601d3494513d9b23
JEFFors <33591723+jeffors@users.noreply.github.com>
2025-07-01 19:31:59 +0500

    Add simple reducers in profile slice

--- src/services/slices/profileSlice.ts
+++ src/services/slices/profileSlice.ts
@@ -5,17 +5,43 @@ import type { Profile } from '../../utils/types';
 type ProfileState = {
   profile: Profile | null;
   isAuth: boolean;
+  // Temporary, save in localstorage later
+  accessToken: string | null;
 };
 
 const initialState: ProfileState = {
   profile: null,
   isAuth: false,
+  accessToken: null
 };
 
 export const profileSlice = createSlice({
   name: 'profile',
   initialState,
-  reducers: {}
+  reducers: {
+    setProfile: (state, action) => {
+      state.profile = action.payload;
+      state.isAuth = true;
+      state.accessToken = 'mock.access.jwt';
+    },
+    updateProfile: (state, action) => {
+      if (state.profile) {
+        state.profile = { ...state.profile, ...action.payload };
+      }
+    },
+    clearProfile: (state) => {
+      state.profile = null;
+      state.isAuth = false;
+    }
+  },
+  selectors: {
+    selectProfile: (state) => {
+      state.profile;
+    },
+    selectIsAuth: (state) => {
+      state.isAuth;
+    }
+  }
 });
 
 export default profileSlice.reducer;
