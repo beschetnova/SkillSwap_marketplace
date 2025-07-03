8d6129a3500ba3edd7acc03bc93b2314a10ee31f
beschetnova <bes4etnova.an@yandex.ru>
2025-07-03 15:41:59 +0300

    add ProfilePage without functionality

---
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,16 @@
+import Header from '../../components/ui/header/header.tsx';
+import ProfilePageUI from '../../components/ui/pages/ProfilePage/ProfilePage.tsx';
+import Footer from '../../components/footer/footer.tsx';
+
+
+const ProfilePage = () => {
+  return (
+    <>
+      <Header />
+      <ProfilePageUI />
+      <Footer />
+    </>
+  );
+};
+
+export default ProfilePage;
