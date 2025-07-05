a830f662fbb33fd87afbbe0062d520dcd8560b37
beschetnova <bes4etnova.an@yandex.ru>
2025-07-05 20:49:20 +0300

    add filter to DropDownSkillsList

--- src/components/ui/header/DropDownSkillsList/DropDownSkillsList.tsx
+++ src/components/ui/header/DropDownSkillsList/DropDownSkillsList.tsx
@@ -1,16 +1,34 @@
 import ListCategory from '../ListCategory/ListCategory';
 import styles from './dropSownSkillsList.module.css';
+import { toggleSkill, selectFilters} from '../../../../services/slices/filtersSlice';
+import { useAppDispatch, useAppSelector} from '../../../../utils/hooks';
 
 interface DropDownSkillsListProps {
-  skillsList: Array<{ id: string; name: string; icon: string; skills: Array<{ id: string; name: string }> }>;
+  skillsList: Array<{ 
+    id: string; 
+    name: string; 
+    icon: string; 
+    skills: Array<{ id: string; name: string }> 
+  }>;
 }
 
 const DropDownSkillsList: React.FC<DropDownSkillsListProps> = ({ skillsList }) => {
+  const dispatch = useAppDispatch();
+  const filters = useAppSelector(selectFilters);
+  
+  const onSkillClick = (skillId: string) => {
+    dispatch(toggleSkill(skillId));
+  };
+
   return (
     <ul className={styles.categoryList}>
       {skillsList.map((item) => (
         <li key={item.id} className={styles.singleCategory}>
-          <ListCategory item={item} />
+          <ListCategory 
+            item={item} 
+            onSkillClick={onSkillClick}
+            activeSkills={filters.skills}
+          />
         </li>
       ))}
     </ul>
