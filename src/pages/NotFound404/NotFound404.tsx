52a27a8a2c2f09cf84835a61aa004deec795f173
JEFFors <33591723+jeffors@users.noreply.github.com>
2025-07-06 17:00:24 +0500

    Add links to home page

--- src/pages/NotFound404/NotFound404.tsx
+++ src/pages/NotFound404/NotFound404.tsx
@@ -1,9 +1,13 @@
+import { useNavigate } from 'react-router-dom';
 import NotFound404UI from '../../components/ui/NotFound404/NotFound404';
 import styles from './NotFount404.module.css';
 
 export const NotFound404 = () => {
+  const navigate = useNavigate();
+
   const redirectHandle = () => {
     console.log('redirect to home page');
+    navigate('/');
   };
 
   const errorMessageHandle = () => {
