214e41c4f91aea9c4684bfbffe5416bc0ea3761a
JEFFors <33591723+jeffors@users.noreply.github.com>
2025-07-07 23:24:43 +0500

    Fix gender choose

--- src/utils/schemas/registrationSchemas.ts
+++ src/utils/schemas/registrationSchemas.ts
@@ -1,5 +1,8 @@
 import z from 'zod';
 
+const max_image_size = 2 * 1024 * 1024;
+const image_types = ['image/jpeg', 'image/jpg', 'image/png'];
+
 export const stepOneSchema = z.object({
   email: z.string().email({ message: 'Неверный формат эл. почты' }),
   password: z
@@ -9,12 +12,21 @@ export const stepOneSchema = z.object({
 
 // заглушки
 export const stepTwoSchema = z.object({
-  // userPic: нужно сделать
+  // avatar: z
+  //   .instanceof(File, {message: 'Требуется фотография'})
+  //   .refine(
+  //     (file) => file.size <= max_image_size,
+  //     'Максимальный размер фотографии - 2 МБ'
+  //   )
+  //   .refine(
+  //     (file) => image_types.includes(file.type),
+  //     'Доступны фотографии только JPEG и PNG формата'
+  //   ),
   name: z.string().nonempty({ message: 'Требуется указать имя' }),
   birthDate: z.date({
     required_error: 'Укажите дату рождения'
   }),
-  gender: z.string().optional(),
+  gender: z.string().nonempty({message: 'Укажите пол'}),
   city: z.string().nonempty({message: 'Требуется указать город'}),
   categoryToLearn: z.string().nonempty({message: 'Требуется указать категорию'}),
   subcategoryToLearn: z.string().nonempty({message: 'Требуется указать подкатегорию'})
