fc87709c8c726bd6eb7b7f30aee183a08a5369ee
Vadim Maslov <dub4ex@mail.ru>
2025-07-10 21:17:00 +0300

    add smooth redirect

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,13 @@
+import { useNavigate } from 'react-router-dom';
 import FooterUI from '../ui/footer/footer';
 
 export const Footer = () => {
-  return <FooterUI />;
+  const navigate = useNavigate();
+  const handleLogoClick = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    navigate('/');
+  };
+  return <FooterUI handleLogoClick={handleLogoClick} />;
 };
 
 export default Footer;
