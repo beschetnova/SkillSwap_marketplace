ff3dcaf8d2cb6e703e93923b19100c55a667b094
JEFFors <33591723+jeffors@users.noreply.github.com>
2025-07-10 21:45:28 +0500

    Add message if favs empty

--- src/pages/FavoritesPage/FavoritesPage.tsx
+++ src/pages/FavoritesPage/FavoritesPage.tsx
@@ -4,6 +4,10 @@ import { selectAllUsers } from '../../services/slices/usersSlice.ts';
 import { UserCardSection } from '../../components/UserCardSection/UserCardSection.tsx';
 import { selectAllSkills } from '../../services/slices/skillsSlice.ts';
 import styles from './FavoritesPage.module.css';
+import Button from '../../components/ui/buttons/button.tsx';
+import icon from '../../images/icons/like2.svg';
+import { Link } from 'react-router-dom';
+import PathConstants from '../../routes/path-constants.ts';
 
 export const FavoritesPage = () => {
   const favorites = useAppSelector(selectFavorites);
@@ -11,15 +15,30 @@ export const FavoritesPage = () => {
 
   const favoriteUsers = allUsers.filter((user) => favorites.includes(user.id));
   const categories = useAppSelector(selectAllSkills);
+  const hasUsers = favoriteUsers.length !== 0;
 
   return (
     <main className={styles.favorites}>
-      <UserCardSection
+      {!hasUsers && (
+        <div className={styles.empty}>
+          <img className={styles.icon} src={icon} alt='Icon' />
+          <div className={styles.description}>
+            <h2>В избранных никого нет</h2>
+            <p>Добавьте в избранные пользователей с главного страницы</p>
+          </div>
+          <Link to={PathConstants.HOME}>
+            <Button fullWidth={true} type='primary'>
+              На главную страницу
+            </Button>
+          </Link>
+        </div>
+      )}
+      {hasUsers && <UserCardSection
         title='Избранное'
         users={favoriteUsers}
         categories={categories}
         toShowAll={true}
-      />
+      /> }
     </main>
   );
 };
